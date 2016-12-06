a39201d3879de59757e8633e16eae4566efd0927
ivanturianytsia <vanyaturianitsa@gmail.com>
2016-12-06 13:04:23 +0100

    server setup

--- ansible_setup.sh
+++ ansible_setup.sh
@@ -5,3 +5,7 @@ docker exec -i -t ansible /bin/bash
 docker exec --tty 92e3799d72f6 env TERM=xterm ansible all -m shell -a 'ls'
 docker exec --tty 92e3799d72f6 env TERM=xterm ansible-playbook /playbooks/cli_variables.yaml
 docker exec --tty 92e3799d72f6 env TERM=xterm ansible all -m shell -a 'cp /shared/cli_variables.yaml /playbooks/cli_variables.yaml'
+
+export DOCKER_HOST=207.244.96.79:2376
+export DOCKER_TLS_VERIFY=1
+export DOCKER_CERT_PATH=/Users/vanyaturianitsa/Documents/Projects/WORK/DecemberCourses/DockerSwarmVideo/MY/keys
